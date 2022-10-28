40c8963ec9748b67714b9b4ef66ed26a5e73801a
Playmaker1210 <hatvanitamas09@gmail.com>
2022-10-28 18:26:13 +0200

    Basic camera movement

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Camera mainCamera;
+    public Vector3 offset;
+
+
+    public void moveCameraToTarget(Transform target) {
+        Debug.Log("Moved to " + target.name);
+        mainCamera.transform.position = target.position + offset;
+        mainCamera.orthographicSize = 3f;
+    }
+
+    public void moveCameraBack() {
+        Debug.Log("Moved back");
+        mainCamera.transform.position = new Vector3(0, 0, -10);
+        mainCamera.orthographicSize = 10.3f;
+    }
+}
