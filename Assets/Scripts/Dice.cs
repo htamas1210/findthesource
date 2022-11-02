39b73cf5a838d13fcf3caf0a6a7e4587f55da449
Playmaker1210 <hatvanitamas09@gmail.com>
2022-11-02 13:15:47 +0100

    valasztott szam az akciok mezobe megjelenik

--- Assets/Scripts/Dice.cs
+++ Assets/Scripts/Dice.cs
@@ -2,33 +2,44 @@
 using UnityEngine;
 
 public class Dice : MonoBehaviour {
-
     public Sprite[] diceSides = new Sprite[6];
-
     public SpriteRenderer hely1;
     public SpriteRenderer hely2;
+    public SpriteRenderer hely3;
 
     private int[] diceResult = { 0, 0 };
     public int valasztottErtek; //a jatekos altal valasztott dobott ertek helye
-
     private bool locked = false; //ne lehessen ujra kivalasztani a masikat ha mar tortent egy valasztas
 
-    public void ertekValasztas(GameObject gomb) {      
+    //getters setters
+    public int[] getDices() { return diceResult; }
+    public int getValasztottErtek() { return valasztottErtek; }
+    public void setValasztottErtek(int ujErtek) { valasztottErtek = ujErtek; }
+
+
+    public void ertekValasztas(GameObject gomb) {
         if (diceResult[0] != 0 && diceResult[1] != 0 && !locked) { //megnezzuk hogy lett e mar dobva es nem valasztott meg a jatekos
-            if (gomb.name == "dice1btn") { 
+            if (gomb.name == "dice1btn") {
                 valasztottErtek = diceResult[0];
+
+                //a valasztott szam atirasa az akcio mezobe
+                hely3.sprite = diceSides[valasztottErtek-1];
+                hely3.size = new Vector2(38, 38);
+
                 locked = true;
-            }else if (gomb.name == "dice2btn") {
+            } else if (gomb.name == "dice2btn") {
                 valasztottErtek = diceResult[1];
+                
+                //a valasztott szam atirasa az akcio mezobe
+                hely3.sprite = diceSides[valasztottErtek-1];
+                hely3.size = new Vector2(38, 38);
+
                 locked = true;
             }
         }
 
         Debug.Log("valasztott ertek: " + valasztottErtek + "locked status: " + locked);
     }
-
-    public int[] getDices() { return diceResult; }
-
 
     private int RollDice(SpriteRenderer renderer) {
         int randomDiceSide = Random.Range(0, 5);
