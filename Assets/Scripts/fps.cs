34bfdeb04390ebe82d01166174fb074e20de250d
Playmaker1210 <hatvanitamas09@gmail.com>
2022-11-11 19:19:54 +0100

    switched to URP

--- Assets/Scripts/fps.cs
+++ Assets/Scripts/fps.cs
@@ -2,15 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System.Threading;
 
 public class fps : MonoBehaviour
 {
     public TMP_Text fpsText;
+    [SerializeField] private float _hudRefreshRate = 1f;
+    private float _timer;
 
     // Update is called once per frame
     void Update()
     {
-        int current = (int)(1f / Time.unscaledDeltaTime);
-        fpsText.text = "Fps: " + current.ToString();
+        /*int current = (int)(1f / Time.unscaledDeltaTime);
+        fpsText.text = "Fps: " + current.ToString();*/
+
+        if (Time.unscaledTime > _timer) {
+            int fps = (int)(1f / Time.unscaledDeltaTime);
+            fpsText.text = "FPS: " + fps;
+            _timer = Time.unscaledTime + _hudRefreshRate;
+        }
+
     }
 }
