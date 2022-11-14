b621877eb27f80ee0943c79c77e5d58264128df2
Playmaker1210 <hatvanitamas09@gmail.com>
2022-11-14 15:01:48 +0100

    ap koltseg hozzaadva a betarazashoz

--- Assets/Scripts/Akciok.cs
+++ Assets/Scripts/Akciok.cs
@@ -10,24 +10,33 @@ public class Akciok : MonoBehaviour
     private int tolteny_index = 3;
     private int felhasznalt_tolteny = 0;
     public int betarazott_tolteny = 3;
+    private Akciopont ap;
     //
 
     private void Start() {
+        ap = FindObjectOfType<Akciopont>();
+
         //kezdesnel karikazza be az elso harom adott toltenyt
         for (int i = 0; i < tolteny_index; i++) {
             toltenyek[i].text = "O";
         }
     }
 
-    public void Betarazas() {
-        if (tolteny_index < 24) {
+    public void Betarazas(int betarazas) {
+        if (tolteny_index < 24 && ap.akciopont != 0) {
+            if(tolteny_index + betarazas > 23) {
+                tolteny_index = 23;
+            } else {
+                tolteny_index += betarazas;
+            }
             //felhasznalt toltenytol megy hogy az ott levo x-et ne irja felul
-            for (int i = felhasznalt_tolteny; i <= tolteny_index; i++) {
+            for (int i = felhasznalt_tolteny; i < tolteny_index; i++) {
                 toltenyek[i].text = "O";
             }
 
-            betarazott_tolteny++;
-            tolteny_index++;
+            betarazott_tolteny += betarazas;
+            //tolteny_index++;
+            ap.akciopont--;
 
             Debug.Log("Tolteny betarazva");
             Debug.Log("Betarazott toltenyek szama: " + betarazott_tolteny);
