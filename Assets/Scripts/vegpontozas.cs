288713ee941e1e9673da6ae725a6d4b39ae27ade
playmaker1210 <hatvanitamas09@gmail.com>
2022-11-16 12:25:33 +0100

    update

--- Assets/Scripts/vegpontozas.cs
+++ Assets/Scripts/vegpontozas.cs
@@ -8,29 +8,39 @@ public class vegpontozas : MonoBehaviour
 {
     private Energia energia;
     private Upgrade FejlesztesPont;
+    private Targyak targyak;
+    private Akciok akciok;
     public bool foundsource;
+    private Elet elet;
 
     int osszpont = 0;
-    int elet = 5;
     int elhasznaltelet = 0;
     int tolteny = 0;
     int megszerzetttargyak = 0;
+    int fejlesztespont;
+    int energiapont;
+    int megmaradtelet;
     // Start is called before the first frame update
     void Start()
     {
         energia = FindObjectOfType<Energia>();
         energia = FindObjectOfType<Energia>();
         FejlesztesPont = FindObjectOfType<Upgrade>();
-        
+        targyak = FindObjectOfType<Targyak>();
+        akciok = FindObjectOfType<Akciok>();
+        elet = FindObjectOfType<Elet>();
         foundsource = false;
-
-        int energiapont = Convert.ToInt32(energia.energiasav_text) - energia.getEnergiasavIndex();
-        int fejlesztespont = FejlesztesPont.fejlesztes_szamlalo/3;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //osszpont = /*energiapont +*/ ((elet - elhasznaltelet) * 2) + FejlesztesPont + tolteny + megszerzetttargyak;
+        tolteny = akciok.betarazott_tolteny;
+        megszerzetttargyak = targyak.targy_szamlalo;
+        energiapont = Convert.ToInt32(energia.energiasav_text) - energia.getEnergiasavIndex();
+        fejlesztespont = FejlesztesPont.fejlesztes_szamlalo;
+        megmaradtelet = elet.elet;
+
+        osszpont = (energiapont * 2) + (megmaradtelet * 2) + (fejlesztespont / 3) + (tolteny / 2) + megszerzetttargyak;
     }
 }
